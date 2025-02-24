2fba87a4812186dece2a6556d6cabc422d6486e2
Gunjankher <gkhercode@gmail.com>
2025-02-24 17:17:23 +0530

    making a controller

--- shopping-list/src/App.jsx
+++ shopping-list/src/App.jsx
@@ -4,13 +4,74 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [product, setProduct] = useState('')
+  const [products, setProducts] = useState([])
+
+
+const addProduct = ()=>{
+  if(product?.trim() !== ''){
+setProducts([...products,{product:product, bought:false}])
+setProduct('')
+  }
+}
+
+
+
+const deleteProduct = (product)=>{
+setProducts(products.filter((item)=> item.product !== product))
+}
+
+
 
   return (
     <>
      <h1>Shopping list app</h1>
+
+<input 
+type="text" 
+placeholder='add your shopping list here'
+value={product}
+style={{padding:'10px' ,margin:'20px'}}
+onChange={(e)=>setProduct(e.target.value)}
+/>
+
+<button onClick={()=> setProducts(addProduct)}>add Product</button>
+
+
+
+<div>
+{
+  products?.map((item,index)=>(
+    <div key={index}>
+<span>{item.product}</span>
+<button onClick={()=>deleteProduct(item.product)} style={{margin :"20px"}}>delete</button>
+    </div>
+  ))
+}
+</div>
+
+
     </>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+ /*
+ shopping app 
+  
+ add product 
+ delete product
+ buy unbuy product 
+ 
+ */
+
+
