b49bbf77c66764333edfb110c497d5780f63ba34
Gunjankher <gkhercode@gmail.com>
2025-03-08 15:34:24 +0530

    react-router-dom

--- react-router-project/src/App.jsx
+++ react-router-project/src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1>This is a react-router Project</h1>
     </>
   )
 }
 
 export default App
+
--- react-router-project/src/main.jsx
+++ react-router-project/src/main.jsx
@@ -2,9 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Layout from './Layout.jsx'
+import {createBrowserRouter,createRoutesFromElements,Route,Router,RouterProvider} from 'react-router-dom'
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element = {<Layout/>} >
+
+
+<Route  path='home' element= {<Home />}/>
+  <Route  path='about' element= {<About />}/>
+  <Route  path='Contact' element= {<Contact />}/>
+  <Route  path='user/:userid' element= {<User />}/>
+    </Route >
+  )
+)
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </StrictMode>,
 )
