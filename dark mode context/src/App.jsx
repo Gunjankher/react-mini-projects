41159c7561282f014b32b446589f85e5245d8010
Gunjankher <gkhercode@gmail.com>
2025-03-10 16:38:53 +0530

    dark mode context finished

--- dark mode context/src/App.jsx
+++ dark mode context/src/App.jsx
@@ -1,15 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { ThemeContext, ThemeProvider } from './context/context-theme'
+import { useEffect } from 'react'
+import ThemeBtn from './components/ThemeCard'
+import Card from './components/Card'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState('light')
+
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+}
+
+
+useEffect(()=>{
+document.querySelector('html').classList.remove('light','dark')
+document.querySelector('html').classList.add(themeMode)
+},[themeMode])
 
   return (
-    <>
-      <h1 className='text-red-400'>Dark mode context</h1>
-    </>
+    <ThemeProvider value={{themeMode,darkTheme,lightTheme}}>
+        <div className="flex flex-wrap min-h-screen items-center">
+			          <div className="w-full">
+			              <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+			                  <ThemeBtn />
+			              </div>
+			
+			              <div className="w-full max-w-sm mx-auto">
+			                  <Card />
+			              </div>
+			          </div>
+			      </div>
+    </ThemeProvider>
   )
 }
 
