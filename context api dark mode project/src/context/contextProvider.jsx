fc87cca346048ab19129a7ff3da660d9ace1d797
Gunjankher <gkhercode@gmail.com>
2025-03-10 15:14:47 +0530

    dark mode context api

---
+++ context api dark mode project/src/context/contextProvider.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import userContext from "./useContext";
+
+
+const userContextProvider = ({children})=>{
+
+    const [user,setUser] = React.useState(null)
+
+return (
+    <userContext.Provider value = {{user,setUser}}>
+{children}
+    </userContext.Provider>
+)
+
+}
+
+
+export default userContextProvider
